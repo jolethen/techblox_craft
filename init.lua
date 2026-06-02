16ad040f80d268068d27f409d5de159aa4421ea1
jolethen <155651557+jolethen@users.noreply.github.com>
2026-06-02 17:22:26 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,3 +2,4 @@ local modpath = core.get_modpath(core.get_current_modname())
 
 core.dofile(modpath .. "/recipes/forgotten_monsters.lua")
 core.dofile(modpath .. "/recipes/enderpearl.lua")
+core.dofile(modpath .. "/recipes/animalia.lua")
