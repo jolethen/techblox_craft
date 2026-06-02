941e74134bbe19ca75792144618d8187133b3900
jolethen <155651557+jolethen@users.noreply.github.com>
2026-06-02 17:21:22 +0530

    Create animalia.lua

---
+++ recipes/animalia.lua
@@ -0,0 +1,8 @@
+core.register_craft({
+	output = "animalia:nametag",
+	recipe = {
+		{"", "farming:string", ""}, 
+		{"default:paper", "default:paper", ""}, 
+		{"default:paper", "default:paper", ""},
+	}
+})
