e95aafe1811acbae9e60c48cc8c32cd75ea71caf
jolethen <155651557+jolethen@users.noreply.github.com>
2026-07-07 15:42:49 +0530

    Update forgotten_monsters.lua

--- recipes/forgotten_monsters.lua
+++ recipes/forgotten_monsters.lua
@@ -1,8 +1,13 @@
+-- 1. Clear the existing recipe for the translocation rod
+core.clear_craft({
+	output = "forgotten_monsters:translocation_rod",
+})
+
 core.register_craft({
 	output = "forgotten_monsters:translocation_rod",
 	recipe = {
 		{"", "forgotten_monsters:spectrum_orb_block", ""}, 
-		{"", "forgotten_monsters:eye_of_the_golem", ""}, 
-		{"", "group:stick", ""},
+		{"", "forgotten_monsters:eye_of_the_golem",   ""}, 
+		{"", "group:stick",                          ""},
 	}
 })
