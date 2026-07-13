0becc84ba97eaa06be160c11455ee8f39cad41f4
jolethen <155651557+jolethen@users.noreply.github.com>
2026-07-13 22:18:57 +0530

    Update khopesh.lua

--- recipes/khopesh.lua
+++ recipes/khopesh.lua
@@ -1,3 +1,9 @@
+-- Step 1: Clear any existing recipes for the khopesh
+minetest.clear_craft({
+    output = "medieval_weapons:khopesh",
+})
+
+-- Step 2: Register your new custom recipe
 minetest.register_craft({
     output = "medieval_weapons:khopesh",
     recipe = {
