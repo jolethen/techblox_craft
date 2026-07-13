863fe78f4c7939a47d0ebace7ce3785c21aaf7b9
jolethen <155651557+jolethen@users.noreply.github.com>
2026-07-13 18:05:16 +0530

    Load new wands recipe and update existing recipes
    Added a new recipe file for wands and updated the loading of existing recipe files.

--- init.lua
+++ init.lua
@@ -1,7 +1,8 @@
 local modpath = core.get_modpath(core.get_current_modname())
 
-core.dofile(modpath .. "/recipes/forgotten_monsters.lua")
-core.dofile(modpath .. "/recipes/enderpearl.lua")
-core.dofile(modpath .. "/recipes/animalia.lua")
-core.dofile(modpath .. "/recipes/eandlblocks.lua")
-core.dofile(modpath .. "/recipes/eocraft.lua")
+dofile(modpath .. "/recipes/forgotten_monsters.lua")
+dofile(modpath .. "/recipes/enderpearl.lua")
+dofile(modpath .. "/recipes/animalia.lua")
+dofile(modpath .. "/recipes/eandlblocks.lua")
+dofile(modpath .. "/recipes/eocraft.lua")
+dofile(modpath .. "/recipes/wands.lua")
