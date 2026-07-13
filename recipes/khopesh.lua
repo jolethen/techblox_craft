cc0ad177c60afa243bcfac560fc0fe9e07311f2b
jolethen <155651557+jolethen@users.noreply.github.com>
2026-07-13 22:17:36 +0530

    Create khopesh.lua

---
+++ recipes/khopesh.lua
@@ -0,0 +1,8 @@
+minetest.register_craft({
+    output = "medieval_weapons:khopesh",
+    recipe = {
+        {"clorofita:LDC", "clorofita:LDC", ""},
+        {"", "clorofita:LDC", ""},
+        {"", "", "medieval_weapons:hilt"}
+    }
+})
